f03806e4a0282da9df788a95f8ca59de4a3c32f7
Andrew <andrew.melauda@gmail.com>
2025-09-04 20:18:09 +0200

    library edded

--- vars/DeployToDev.groovy
+++ vars/DeployToDev.groovy
@@ -1,21 +1,13 @@
 def call(Map config = [:]){
-pipeline {
-    agent any
-    stages {
-        stage('deploy') {
-            steps {
-                script {
-                        def containerStatus = sh(script: "docker ps -a --filter name=^/${config.conteinerName}\$ --format '{{.Names}}'", returnStdout: true).trim()
+
+ def containerStatus = sh(script: "docker ps -a --filter name=^/${config.conteinerName}\$ --format '{{.Names}}'", returnStdout: true).trim()
                         
-                        if (containerStatus == containerName) {
-                            sh "docker container stop ${config.conteinerName} || true"
-                            sh "docker container rm -f ${config.conteinerName}"
-                        } 
-                        withDockerRegistry(credentialsId: 'DOCKER_HUB', url: '') {
-			sh "docker run -d --name ${config.conteinerName} -p ${config.port}:3000 andrdud/${config.conteinerName}:${config.tag}"}
-                }
-            }
-        }
+ if (containerStatus == containerName) {
+    sh "docker container stop ${config.conteinerName} || true"
+    sh "docker container rm -f ${config.conteinerName}"
+    } 
+withDockerRegistry(credentialsId: 'DOCKER_HUB', url: '') {
+    sh "docker run -d --name ${config.conteinerName} -p ${config.port}:3000 andrdud/${config.conteinerName}:${config.tag}"
     }
-}
+
 }
